45b843f77fe22f82e0cb137b1824b6d989b49d04
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-10 00:14:53 +0530

    Main App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,35 @@
 import React from 'react'
+import SearchSection from './components/SearchSection';
+import CurrentWeather from './components/CurrentWeather';
+import HourlyWeatherItem from './components/HourlyWeatherItem';
 
 const App = () => {
+  const getWeatherDetails = async (API_URL) => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
   return (
   <div className='container'>
     {/* Search Section */}
-    <div className='search-section'>
-      <form action='#' className='search-form'>
-      <span class="material-symbols-rounded">search</span>
-        <input type="search" placeholder='Enter a city name' className="search-input"  required/>
-      </form>
-      <button className="location-button">
-      <span class="material-symbols-rounded">my_location</span>
-      </button>
-    </div>
+    <SearchSection getWeatherDetails={getWeatherDetails} />
 
     {/* Weather Section */}
     <div className='weather-section'>
-      <div className='current-weather'>
-        <img src="icons/clouds.svg" alt="" className="weather-icon" />
-        <h2 className="temperature">20<span>°C</span></h2>
-        <p className="description">Partly Cloudy</p>
-      </div>
+     <CurrentWeather/>
 
       {/* Hourly Weather Forecast List Section */}
       <div className="hourly-forecast">
         <ul className="weather-list">
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
-
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
-
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
-
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
-
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
-
-          <li className="weather-item">
-            <p className="time">00:00</p>
-            <img src="icons/clouds.svg" alt="" className="weather-icon" />
-            <p className="temperature">20°</p>
-          </li>
+          <HourlyWeatherItem />
         </ul>
       </div>
-
-
 
     </div>
   </div> 
