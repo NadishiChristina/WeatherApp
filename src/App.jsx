6526841c6b1019918d6b00704e44ced7bcc1d19d
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-12 14:42:50 +0530

    Display the weatherIcon to match the weather

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import SearchSection from './components/SearchSection';
 import CurrentWeather from './components/CurrentWeather';
 import HourlyWeatherItem from './components/HourlyWeatherItem';
+import { weatherCodes } from './constants';
 
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState({});
@@ -15,8 +16,9 @@ const App = () => {
       
       const temperature = Math.floor(data.current.temp_c);
       const description = data.current.condition.text;
+      const weatherIcon = Object.keys(weatherCodes).find((icon) => weatherCodes[icon].includes(data.current.condition.code));
 
-      setCurrentWeather({ temperature, description });
+      setCurrentWeather({ temperature, description, weatherIcon });
 
     } catch (error) {
       console.log(error);
--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CurrentWeather = ({currentWeather}) => {
   return (
     <div className='current-weather'>
-    <img src="icons/clouds.svg" alt="" className="weather-icon" />
+    <img src={`icons/${currentWeather.weatherIcon}.svg`} alt="" className="weather-icon" />
     <h2 className="temperature">{currentWeather.temperature}<span>°C</span></h2>
     <p className="description">{currentWeather.description}</p>
   </div>
